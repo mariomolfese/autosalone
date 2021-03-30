077b47d60f0f5ed71de3a48bd9e7d9d76c005830
mario <mario@molfese.net>
2021-03-30 11:39:57 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,1 +1,9 @@
+<?php
+//array associativo $CONFIG, non è built-in
 
+	$CONFIG["db-host"]	= "localhost";	// cioè il server remoto. LE PAGINE PHP LO VEDONO COME LOCALE
+	$CONFIG["db-name"]	= "my_molfese"; 
+	$CONFIG["db-user"]	= "molfese";
+	$CONFIG["db-pswd"]	= "";			      // altervista, bontà sua, non ci chiede la password
+    									                // se ci connettiamo da localhost
+?>
