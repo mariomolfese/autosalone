fe1f3cfedd54dbf4bfd4b00daf6ee8c7255d3410
mario <mario@molfese.net>
2021-04-01 12:13:57 +0200

    Add files via upload

---
+++ db/as_registrati.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS `as_registrati` (
+  `pk` bigint(20) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `cognome` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `email` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `is_enabled` tinyint(4) NOT NULL DEFAULT '1',
+  `note` text CHARACTER SET utf8,
+  PRIMARY KEY (`pk`)
+);
+
+INSERT INTO `as_registrati` (`pk`, `nome`, `cognome`, `email`, `password`, `is_enabled`, `note`) VALUES
+(1, 'mario', 'molfese', 'mario@molfese.net', 'd8ed30d057c6a36fa6788597939826e1', 1, 'la password in chiaro è "amilcare"');
